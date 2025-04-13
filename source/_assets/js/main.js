cb64b7b1d5e73be9aa13ad13e54b9305ded3cf8a
terremoth <dutra.astro@gmail.com>
2025-04-13 10:03:40 -0300

    center modal dialog newsletter

--- source/_assets/js/main.js
+++ source/_assets/js/main.js
@@ -98,7 +98,7 @@ document.getElementById('subscribe-newsletter-btn').addEventListener('click', ev
 
     const modal = `
         <div class="modal fade" id="subscribe-modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
+            <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h1 class="modal-title fs-5" id="exampleModalLabel">Successfully Subscribed</h1>
